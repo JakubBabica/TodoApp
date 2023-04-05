bc1eb28fdc89d90cd98726b8398c554ebef6960f
Jakub babica <315190@via.dk>
2023-04-06 00:37:43 +0200

    adding test to project

---
+++ Test/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Application.Logic;
+using Application.LogicInterfaces;
+using Domain.DTOs;
+using FileData;
+using FileData.DAOs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test;
+[TestClass]
+public class Class1
+{
+    [TestMethod]
+    public void testmethod()
+    {
+        ITodoLogic logic = new TodoLogic(new TodoFileDao(new FileContext()),new UserFileDao(new FileContext()));
+        SearchTodoParametersDto dto = new SearchTodoParametersDto("jakub", 1, true, "Help");
+        var expectedErrorMessage = "Start date cannot be before the end date";
+        Task<Exception> ex = Assert.ThrowsExceptionAsync<Exception>(() => logic.GetAsync(dto));
+        Assert.AreEqual(expectedErrorMessage, ex.Result.Message);
+    }
+}
