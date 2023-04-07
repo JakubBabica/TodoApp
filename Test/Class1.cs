c25462ba68eab1b57d86d91770ba55ec4efe26ab
Jakub babica <315190@via.dk>
2023-04-07 08:40:15 +0200

    test

--- Test/Class1.cs
+++ Test/Class1.cs
@@ -13,6 +13,7 @@ public class Class1
     public void testmethod()
     {
         ITodoLogic logic = new TodoLogic(new TodoFileDao(new FileContext()),new UserFileDao(new FileContext()));
+        
         SearchTodoParametersDto dto = new SearchTodoParametersDto("jakub", 1, true, "Help");
         var expectedErrorMessage = "Start date cannot be before the end date";
         Task<Exception> ex = Assert.ThrowsExceptionAsync<Exception>(() => logic.GetAsync(dto));
